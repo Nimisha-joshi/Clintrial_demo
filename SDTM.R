7b2af72468491478bcff80634aee13bedd1b569d
Nimisha-joshi <nims3981@gmail.com>
2026-05-05 13:32:00 +0100

    feat: initial clinical trials portfolio setup

---
+++ SDTM.R
@@ -0,0 +1,9 @@
+library(pharmaversesdtm)
+
+# You now have access to:
+dm      # Demographics
+ae      # Adverse Events  
+vs      # Vital Signs
+lb      # Lab data
+ex      # Exposure
+cm      # Concomitant medications
